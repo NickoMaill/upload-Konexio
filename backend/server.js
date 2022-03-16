bcc0952a8e3eac196418fca30041178e66720d0a
Nicolas-Maillols <nicomaillols@gmail.com>
2022-03-16 14:48:23 +0100

    server can receive some image

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
-const upload = require('multer');
+const multer = require('multer');
+const upload = multer({ dest: 'public/uploads' })
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 8000
 
 app.use(express.static('public'))
+
+app.post('/upload', upload.single("image"), (req, res) => {
+    fs.renameSync(
+        req.file.path,
+        path.join(req.file.destination, req.file.originalname)
+    );
+    res.send("Image receive");
+});
 
 
 app.listen(PORT, () => console.log("Listening on port 8000"));

6ef6d9d064a55f8d022a1bbb587acb0758cf60db
Nicolas-Maillols <nicomaillols@gmail.com>
2022-03-16 14:33:06 +0100

    test displaying img on server.js

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const upload = require('multer');
+
+const PORT = 8000
+
+app.use(express.static('public'))
+
+
+app.listen(PORT, () => console.log("Listening on port 8000"));
